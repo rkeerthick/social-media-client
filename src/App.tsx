361736a0218a2a7a983e6d67b5918de357d4b9b4
rkeerthick <rkeerthick1@gmail.com>
2023-12-28 17:10:53 +0530

    react query and react query devtools added

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,17 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import React from "react";
 import Routing from "routing/Routing";
 
+const queryClient = new QueryClient();
+
 function App() {
-  return <Routing />;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Routing />
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+  );
 }
 
 export default App;
